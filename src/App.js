5357e1751d15c36281b522f367bee66f3818fb73
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-01-09 23:07:18 +0900

    섹션 20: 고급 리덕스 435 ~ 436 useEffect() 사용

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -5,7 +6,19 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  
+  // 435. 리덕스와 함께 사용하는 useEffect
+  // 컴포넌트에 부수 효과(side effect) 논리를 넣어 모든 데이터 변환 논리를 유지하기 좋은 방법 중 하나
+  //  But, useEffect()의 문제점은 초기(즉, 비어 있는) 카트를 백엔드로 보내고 거기에 저장된 모든 데이터를 덮어쓰는 문제 발생!
+  // const cart = useSelector((state) => state.cart);
+
+  // useEffect(() => {
+  //   fetch('https://react-http-6b4e2-default-rtdb.firebaseio.com/cart.json', {
+  //     method: 'PUT',
+  //     body: JSON.stringify(cart),
+  //   });
+  // }, [cart]);
 
   return (
     <Layout>
